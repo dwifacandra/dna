ba9a061619d79ea34f94fe518b3465fb6230c9dc
Aditya Dwifacandra Nugraha <adityadwifacandra.adn@gmail.com>
2025-01-18 12:01:01 +0700

    modify breadcrumb on SkillCategoryResource

--- app/Core/Clusters/Resumes/Resources/SkillCategoryResource/Pages/ManageSkillCategories.php
+++ app/Core/Clusters/Resumes/Resources/SkillCategoryResource/Pages/ManageSkillCategories.php
@@ -4,11 +4,22 @@ namespace App\Core\Clusters\Resumes\Resources\SkillCategoryResource\Pages;
 
 use App\Core\Traits\DefaultOptions;
 use Filament\Resources\Pages\ManageRecords;
-use App\Core\Clusters\Resumes\Resources\SkillCategoryResource;
+use App\Core\Clusters\Resumes\Resources\{SkillResource, SkillCategoryResource};
 
 class ManageSkillCategories extends ManageRecords
 {
     protected static string $resource = SkillCategoryResource::class;
+
+    public function getBreadcrumbs(): array
+    {
+        if (filled($cluster = static::getCluster())) {
+            return $cluster::unshiftClusterBreadcrumbs([
+                SkillResource::getUrl() => SkillResource::getNavigationLabel(),
+                static::getResource()::getNavigationLabel(),
+            ]);
+        }
+        return [];
+    }
 
     protected function getHeaderActions(): array
     {
