7adda19b7831394883911b40daedb65d157e8322
Aditya Dwifacandra Nugraha <adityadwifacandra.adn@gmail.com>
2025-02-03 15:52:40 +0700

    style: resources/views/core/pages/blog/landing-page.blade.php

--- resources/views/core/pages/blog/landing-page.blade.php
+++ resources/views/core/pages/blog/landing-page.blade.php
@@ -24,7 +24,7 @@
                         {{ $project->description }}
                     </p>
                     <p
-                        class="inline-flex items-center mt-4 text-sm font-medium text-blue-600 gap-x-1 decoration-2 group-hover:underline group-focus:underline dark:text-blue-500">
+                        class="inline-flex items-center mt-4 text-sm font-medium gap-x-1 decoration-2 group-hover:underline group-focus:underline">
                         Read more
                         <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
