d741b496b172951ed7431f6c6a2f9760bf41fc39
Aditya Dwifacandra Nugraha <adityadwifacandra.adn@gmail.com>
2025-02-11 22:15:06 +0700

    ci: fix app config

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'name' => env('APP_AUTHOR', 'Laravel'),
+    'author' => env('APP_AUTHOR', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
