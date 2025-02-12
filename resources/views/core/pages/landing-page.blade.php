a36300cb0112fb41a1304b0e6d1e5ff74e6a3293
Aditya Dwifacandra Nugraha <adityadwifacandra.adn@gmail.com>
2025-02-12 18:58:52 +0700

    fix: livewire component linked error

--- resources/views/core/pages/landing-page.blade.php
+++ resources/views/core/pages/landing-page.blade.php
@@ -1,5 +1,5 @@
 <div id="landing-page" class="flex flex-col max-w-screen-xl py-6 mx-auto gap-y-6">
-    <livewire:pages.design.landing-page />
+    <livewire:pages.collection.landing-page />
     <livewire:pages.blog.landing-page />
     <livewire:pages.product.landing-page />
 </div>
